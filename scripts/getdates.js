abe25a277d80be6273cab8b6b99fba5d2911b41f
benja-novoa322 <benjaminnovoa0901@gmail.com>
2025-09-17 19:32:35 -0500

    terminado

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+const currentyear = document.querySelector("#currentyear");
+currentyear.textContent = new Date().getFullYear();
+
+const lastModified = document.lastModified;
+document.querySelector("#lastModified").textContent = lastModified;
+
+
