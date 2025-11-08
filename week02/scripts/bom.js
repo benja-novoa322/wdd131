7c6774a24b74185790142287c012d193fee187ca
benja-novoa322 <benjaminnovoa0901@gmail.com>
2025-11-08 00:40:50 -0500

    terminado:)

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,32 +2,23 @@ const input = document.querySelector("#favchap");
 const button = document.querySelector('#submit');
 const list = document.querySelector("#list");
 
-
-
-//li.textContent = input.value;
-//deletebtn.textContent = "❌";
-//li.append(deletebtn)
-//list.append(li)
-
 button.addEventListener('click', function () {
-    if (input.value.trim() !== '') {
-        const lichp = document.createElement("li");
-        const deletebtn = document.createElement("button");
-
-        lichp.textContent = input.value;
-        deletebtn.textContent = "❌";
-
-        deletebtn.addEventListener('click', function () {
-            list.removeChild(lichp);
-            input.focus();
-        });
-
-        lichp.append(deletebtn);
-        list.append(lichp);
-        input.value = '';
-
-    }
+    if (input.value.trim() !== '')
+        input.focus();
+    const lii = document.createElement('li');
+    const deletebtn = document.createElement('button');
+    lii.textContent = input.value;
+    deletebtn.textContent = '❌';
+    lii.append(deletebtn);
+    list.append(lii);
+    deletebtn.addEventListener('click', function () {
+        list.removeChild(lii);
+        input.focus();
+    });
+    input.value = '';
     input.focus();
 });
+
+
 
 
