bd1908b1d5f14dbc6a5b9bbc738bbcf33f8affdb
benja-novoa322 <benjaminnovoa0901@gmail.com>
2025-11-11 01:23:37 -0500

    it´s done and I DONE

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,3 +3,11 @@ currentyear.textContent = new Date().getFullYear();
 
 const lastModified = document.lastModified;
 document.querySelector("#lastModified").textContent = lastModified;
+
+const hambutton = document.querySelector('#menubutton');
+const navigation = document.querySelector('.navigation');
+
+hambutton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hambutton.classList.toggle('open');
+});
